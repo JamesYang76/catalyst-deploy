d8637fe3cae3b38fbb131bd1f6bc0447663e26c7
JY <james.yang@ackama.com>
2020-01-09 01:58:30 +1300

    Set up Puma, Nginx & Capistrano

--- Capfile
+++ Capfile
@@ -18,5 +18,7 @@ require "capistrano/puma/nginx"
 install_plugin Capistrano::Puma
 install_plugin Capistrano::Puma::Nginx
 
+require 'sshkit/sudo'
+
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -48,7 +48,6 @@ set :rbenv_roles, :all # default value
 # set :ssh_options, verify_host_key: :secure
 # Don't change these unless you know what you're doing
 set :pty,             true
-set :use_sudo,        false
 
 set :config_example_suffix, '.example'
 set :config_files, fetch(:linked_files)
@@ -66,64 +65,11 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
-
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
-
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
-end
 
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
+# bundle exec pumactl -S /home/deploy/catalyst-deploy/shared/tmp/pids/puma.state -F /home/deploy/catalyst-deploy/shared/puma.rb restart
+#
+#
+#  bundle exec puma -C /home/deploy/catalyst-deploy/shared/puma.rb --daemon
